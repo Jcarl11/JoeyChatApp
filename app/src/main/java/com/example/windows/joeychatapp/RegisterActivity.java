d39d34ebd99b67cffbe31fa7f71d1fee191c4f2e
Joey Francisco <joeycarlofrancisco@gmail.com>
2018-06-23 13:24:00 +0800

    Changed Major Designs

--- app/src/main/java/com/example/windows/joeychatapp/RegisterActivity.java
+++ app/src/main/java/com/example/windows/joeychatapp/RegisterActivity.java
@@ -1,12 +1,32 @@
 package com.example.windows.joeychatapp;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class RegisterActivity extends AppCompatActivity {
+import butterknife.BindView;
+
+public class RegisterActivity extends AppCompatActivity
+{
+
+    @BindView(R.id.BTN_SUBMIT)
+    Button BTN_SUBMIT;
+    @BindView(R.id.BTN_CLEAR)
+    Button BTN_CLEAR;
+    @BindView(R.id.ET_DISPLAYNAME)
+    TextInputEditText ET_DISPLAYNAME;
+    @BindView(R.id.ET_EMAIL)
+    TextInputEditText ET_EMAIL;
+    @BindView(R.id.ET_PASSWORD)
+    TextInputEditText ET_PASSWORD;
+    @BindView(R.id.TV_MESSAGE)
+    TextView TB_MESSAGE;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
--- app/src/main/java/com/example/windows/joeychatapp/StartingActivity.java
+++ app/src/main/java/com/example/windows/joeychatapp/StartingActivity.java
@@ -1,13 +1,46 @@
 package com.example.windows.joeychatapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class StartingActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
+public class StartingActivity extends AppCompatActivity
+{
+
+    @BindView(R.id.BTN_LOGIN)
+    Button BTN_LOGIN;
+
+    @BindView(R.id.BTN_CREATEACC)
+    Button BTN_CREATEACC;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.BTN_LOGIN)
+    public void login(View view)
+    {
+        openNextActivity(LoginActivity.class);
+    }
+
+    @OnClick(R.id.BTN_CREATEACC)
+    public void register(View view)
+    {
+        openNextActivity(RegisterActivity.class);
+    }
+
+    private void openNextActivity(Class<?> destination)
+    {
+        Intent send = new Intent(StartingActivity.this, destination);
+        startActivity(send);
     }
 }
